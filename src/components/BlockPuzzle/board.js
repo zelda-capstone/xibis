9ad2b28fa62a5a8a9ca3b8a783767ea6194428db
Simone Pincsak <simone.pincsak@gmail.com>
2020-08-25 12:04:48 -0500

    the icon moves on click!

--- src/components/BlockPuzzle/board.js
+++ src/components/BlockPuzzle/board.js
@@ -5,25 +5,41 @@ import Icon from './icon'
 export default class Board extends React.Component {
   constructor() {
     super()
+    this.state = {
+      iconPosition: [0, 0],
+    }
     this.drawSquare = this.drawSquare.bind(this)
+    this.moveIcon = this.moveIcon.bind(this)
   }
 
-  drawSquare(i, [iconX, iconY]) {
-    const x = i % 8
-    const y = Math.floor(i / 8)
-    const dark = (x + y) % 2 === 1
-    const hasIcon = iconX === x && iconY === y
+  moveIcon(event, x, y) {
+    this.setState((state) => {
+      return {iconPosition: [x, y]}
+    })
+  }
+
+  drawSquare(i, iconPosition) {
+    const iconX = iconPosition[0]
+    const iconY = iconPosition[1]
+    const squareX = i % 8
+    const squareY = Math.floor(i / 8)
+    const dark = (squareX + squareY) % 2 === 1
+    const hasIcon = iconX === squareX && iconY === squareY
     const piece = hasIcon ? <Icon /> : null
 
     return (
-      <div key={i} style={{width: '12.5%', height: '12.5%'}}>
+      <div
+        key={i}
+        style={{width: '12.5%', height: '12.5%'}}
+        onClick={(event) => this.moveIcon(event, squareX, squareY)}
+      >
         <Square dark={dark}>{piece}</Square>
       </div>
     )
   }
 
   render() {
-    const iconPosition = this.props.iconPosition
+    const iconPosition = this.state.iconPosition
     const squares = []
     for (let i = 0; i < 64; i++) {
       squares.push(this.drawSquare(i, iconPosition))
--- src/components/BlockPuzzle/index.js
+++ src/components/BlockPuzzle/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Board from './board'
 
-export default class BlockPuzzle extends React.Component {
-  render() {
-    return (
-      <div style={{margin: '15px', width: '95vh', height: '95vh'}}>
-        <div style={{width: '95%', height: '95%'}}>
-          I'm just trying to render a basic puzzle here, so bear with me, Bubos.
-          <Board iconPosition={[1, 0]} />
-        </div>
+export default function BlockPuzzle() {
+  return (
+    <div style={{margin: '15px', width: '95vh', height: '95vh'}}>
+      <div style={{width: '95%', height: '95%'}}>
+        <Board />
       </div>
-    )
-  }
+    </div>
+  )
 }
