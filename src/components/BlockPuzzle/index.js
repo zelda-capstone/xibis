52e0026a9deb94263d6f79563ecb46d6e9c3629b
Simone Pincsak <simone.pincsak@gmail.com>
2020-08-25 16:52:16 -0500

    starting new branch for draggable

--- src/components/BlockPuzzle/index.js
+++ src/components/BlockPuzzle/index.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import Board from './board'
+import Icon from './icon'
 
 export default function BlockPuzzle() {
   return (
-    <div style={{margin: '15px', width: '95vh', height: '95vh'}}>
+    <div
+      style={{
+        margin: '15px',
+        width: '95vh',
+        height: '95vh',
+        display: 'flex',
+        flexDirection: 'row',
+      }}
+    >
       <div style={{width: '95%', height: '95%'}}>
         <Board />
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '25%',
+          height: '95%',
+        }}
+      >
+        <Icon />
+        <Icon />
+        <Icon />
+        <Icon />
+        <Icon />
+        <Icon />
+        <Icon />
       </div>
     </div>
   )
