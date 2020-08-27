86475484f626db52841332ba4203314788b5af44
Simone Pincsak <simone.pincsak@gmail.com>
2020-08-27 14:04:08 -0500

    it snaps to a grid!

--- src/components/BlockPuzzle/icon.js
+++ src/components/BlockPuzzle/icon.js
@@ -1,13 +1,48 @@
 import React from 'react'
 import Draggable from 'react-draggable'
-import Piece01 from './puzzle-piece-01.svg'
+import Piece01 from './ImageAssets/puzzle-piece-01.svg'
+import Piece02 from './ImageAssets/puzzle-piece-02.svg'
+import AllPieces from './ImageAssets/puzzle-pieces-separate.svg'
+
+export default function Icon() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+      }}
+    >
+      <Draggable grid={[100, 100]} defaultPosition={{x: 0, y: 22}}>
+        <div
+          style={{
+            backgroundImage: `url(${Piece01})`,
+            height: '200px',
+            width: '300px',
+          }}
+        ></div>
+      </Draggable>
+      <Draggable grid={[100, 100]} defaultPosition={{x: 0, y: 22}}>
+        <div
+          style={{
+            backgroundImage: `url(${Piece02})`,
+            height: '100px',
+            width: '500px',
+          }}
+        ></div>
+      </Draggable>
+    </div>
+  )
+}
+
+/*
+this is the one I'm working on, to get all the pieces to render separately (probably based on background image settings)
 
 export default function Icon() {
   return (
     <Draggable>
       <div
         style={{
-          backgroundImage: `url(${Piece01})`,
+          backgroundImage: `url(${AllPieces})`,
           height: '200px',
           width: '300px',
         }}
@@ -15,3 +50,5 @@ export default function Icon() {
     </Draggable>
   )
 }
+
+*/
--- src/components/BlockPuzzle/index.js
+++ src/components/BlockPuzzle/index.js
@@ -19,7 +19,6 @@ export default function BlockPuzzle() {
       <div
         style={{
           display: 'flex',
-          flexDirection: 'row',
         }}
       >
         <Icon />
