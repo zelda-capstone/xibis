2655ecc28f60f056b80da1dea5b9e7892e86fd7a
Simone Pincsak <simone.pincsak@gmail.com>
2020-08-27 14:29:16 -0500

    refactor icon component

---
+++ src/components/BlockPuzzle/ImageAssets/index.js
@@ -0,0 +1,18 @@
+import Piece01 from './puzzle-piece-01.svg'
+import Piece02 from './puzzle-piece-02.svg'
+
+export const PieceObj01 = {
+  index: 1,
+  image: Piece01,
+  width: 300,
+  height: 200,
+}
+
+export const PieceObj02 = {
+  index: 2,
+  image: Piece02,
+  width: 500,
+  height: 100,
+}
+
+export const PiecesArray = [PieceObj01, PieceObj02]
--- src/components/BlockPuzzle/icon.js
+++ src/components/BlockPuzzle/icon.js
@@ -1,10 +1,47 @@
 import React from 'react'
 import Draggable from 'react-draggable'
+/*
 import Piece01 from './ImageAssets/puzzle-piece-01.svg'
 import Piece02 from './ImageAssets/puzzle-piece-02.svg'
 import AllPieces from './ImageAssets/puzzle-pieces-separate.svg'
+*/
+import {PiecesArray} from './ImageAssets/index'
+
+//here's my new attempt!
+export default function Icon() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexFlow: 'row-wrap',
+      }}
+    >
+      {PiecesArray.map((piece) => {
+        return (
+          <Draggable
+            grid={[100, 100]}
+            defaultPosition={{x: 0, y: 22}}
+            key={piece.index}
+          >
+            <div
+              style={{
+                backgroundImage: `url(${piece.image})`,
+                height: `${piece.height}px`,
+                width: `${piece.width}px`,
+              }}
+            ></div>
+          </Draggable>
+        )
+      })}
+    </div>
+  )
+}
+
+/*
+THIS is the component that currently works! just making it more DRY!
 
 export default function Icon() {
+  console.log('what can we access? ', Piece01)
   return (
     <div
       style={{
@@ -33,6 +70,8 @@ export default function Icon() {
     </div>
   )
 }
+
+*/
 
 /*
 this is the one I'm working on, to get all the pieces to render separately (probably based on background image settings)
