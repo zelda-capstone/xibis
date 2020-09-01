db26dc6db930fddcc33e81ee7896032e8156a6af
Simone Pincsak <simone.pincsak@gmail.com>
2020-09-01 12:38:13 -0500

    correct check works!

--- src/components/BlockPuzzle/icon.js
+++ src/components/BlockPuzzle/icon.js
@@ -1,31 +1,12 @@
 import React from 'react'
 import Draggable from 'react-draggable'
 import {PiecesArray} from './ImageAssets/index'
+import singlePiece from './singlePiece'
+import SinglePiece from './singlePiece'
 
 export default class Icon extends React.Component {
   constructor() {
     super()
-    this.state = {
-      pieceStatus: false,
-    }
-    this.handleDrag = this.handleDrag.bind(this)
-  }
-
-  handleDrag(e, ui, position) {
-    console.log('event ', e)
-    console.log('ui ', ui)
-    const currentPosX = ui.lastX
-    const currentPosY = ui.lastY
-    if (currentPosX === position[0] && currentPosY === position[1]) {
-      console.log('correct!')
-      this.setState = {
-        pieceStatus: true,
-      }
-      //that should make the piece 'stick' to the correct location!
-      //maybe add some sort of visual or audio cue to let player know that the piece is set
-    } else {
-      console.log('keep trying!')
-    }
   }
 
   render() {
@@ -39,25 +20,7 @@ export default class Icon extends React.Component {
       >
         {PiecesArray.map((piece, index) => {
           const position = piece.location
-          return (
-            <Draggable
-              grid={[50, 50]}
-              positionOffset={{x: 0, y: 0}}
-              key={index}
-              disabled={this.state.pieceStatus}
-              onStop={(e, ui) => {
-                this.handleDrag(e, ui, position)
-              }}
-            >
-              <div
-                style={{
-                  backgroundImage: `url(${piece.image})`,
-                  height: `${piece.height}px`,
-                  width: `${piece.width}px`,
-                }}
-              ></div>
-            </Draggable>
-          )
+          return <SinglePiece key={index} piece={piece} />
         })}
       </div>
     )
--- src/components/BlockPuzzle/singlePiece.js
+++ src/components/BlockPuzzle/singlePiece.js
@@ -5,21 +5,39 @@ export default class SinglePiece extends React.Component {
   constructor() {
     super()
     this.state = {
-      xPos: 0,
-      yPos: 0,
       correct: false,
+    }
+    this.handleDrag = this.handleDrag.bind(this)
+  }
+
+  handleDrag(e, ui, correctPos) {
+    console.log('event ', e)
+    console.log('ui ', ui)
+    const currentPosX = ui.lastX
+    const currentPosY = ui.lastY
+
+    if (currentPosX === correctPos[0] && currentPosY === correctPos[1]) {
+      console.log('correct!')
+      this.setState({correct: true})
+      console.log('is it correct? ', this.state.correct)
+      //that should make the piece 'stick' to the correct location!
+      //maybe add some sort of visual or audio cue to let player know that the piece is set
+    } else {
+      console.log('keep trying!')
+      console.log('is it correct? ', this.state.correct)
     }
   }
 
   render() {
+    const piece = this.props.piece
+    const correctPos = piece.location
     return (
       <Draggable
         grid={[50, 50]}
         positionOffset={{x: 0, y: 0}}
-        key={index}
-        disabled={this.state.pieceStatus}
+        disabled={this.state.correct}
         onStop={(e, ui) => {
-          this.handleDrag(e, ui, position)
+          this.handleDrag(e, ui, correctPos)
         }}
       >
         <div
@@ -29,7 +47,6 @@ export default class SinglePiece extends React.Component {
             width: `${piece.width}px`,
           }}
         ></div>
-        >
       </Draggable>
     )
   }
