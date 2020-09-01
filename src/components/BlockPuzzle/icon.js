d131fcb44b9978e7fc4fc03b3e34069a6a04a259
Simone Pincsak <simone.pincsak@gmail.com>
2020-09-01 12:06:15 -0500

    having trouble with disabled=true

--- src/components/BlockPuzzle/icon.js
+++ src/components/BlockPuzzle/icon.js
@@ -14,22 +14,19 @@ export default class Icon extends React.Component {
     this.handleDrag = this.handleDrag.bind(this)
   }
 
-  handleDrag(e, ui, piece) {
+  handleDrag(e, ui, position) {
+    console.log('event ', e)
+    console.log('ui ', ui)
     const currentPosX = ui.lastX
     const currentPosY = ui.lastY
-    console.log('x ', currentPosX)
-    console.log('y ', currentPosY)
-    console.log('piece ', e.target)
-    /*
-    const currentPos = [ui.lastX, ui.lastY]
-    if (currentPos === piece.location) {
-      console.log('that is correct!')
-
+    if (currentPosX === position[0] && currentPosY === position[1]) {
+      console.log('correct!')
+      e.target.disabled = true
+      //that should make the piece 'stick' to the correct location!
+      //maybe add some sort of visual or audio cue to let player know that the piece is set
+    } else {
+      console.log('keep trying!')
     }
-    */
-    //if location === correctLocation then set piece.disabled = true
-    //that should make the piece 'stick' to the correct location!
-    //maybe add some sort of visual or audio cue to let player know that the piece is set
   }
 
   render() {
@@ -42,18 +39,17 @@ export default class Icon extends React.Component {
         }}
       >
         {PiecesArray.map((piece, index) => {
+          const position = piece.location
           return (
             <Draggable
               grid={[50, 50]}
               positionOffset={{x: 0, y: 0}}
               key={index}
-              onStop={(e, ui, piece) => {
-                this.handleDrag(e, ui, piece)
+              onStop={(e, ui) => {
+                this.handleDrag(e, ui, position)
               }}
-              value={index}
             >
               <div
-                value={piece.location}
                 style={{
                   backgroundImage: `url(${piece.image})`,
                   height: `${piece.height}px`,
