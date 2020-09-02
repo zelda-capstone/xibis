7228a6cbabf5b0cfdb64fafb4fe4aa264cfb743a
Simone Pincsak <simone.pincsak@gmail.com>
2020-09-02 15:07:09 -0500

    win text appears

--- src/components/BlockPuzzle/allPieces.js
+++ src/components/BlockPuzzle/allPieces.js
@@ -1,41 +1,19 @@
 import React from 'react'
 import {PiecesArray} from './ImageAssets/index'
 import SinglePiece from './singlePiece'
-import {connect} from 'react-redux'
 
-export class AllPieces extends React.Component {
-  constructor() {
-    super()
-    this.youWin = this.youWin.bind(this)
-  }
-
-  youWin() {
-    console.log('congratulations, you won!')
-  }
-
-  render() {
-    const winCondition = this.props.totalCorrect === 12
-    if (winCondition) {
-      this.youWin()
-    }
-    return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-        }}
-      >
-        {PiecesArray.map((piece, index) => {
-          return <SinglePiece key={index} piece={piece} />
-        })}
-      </div>
-    )
-  }
+export default function AllPieces() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+      }}
+    >
+      {PiecesArray.map((piece, index) => {
+        return <SinglePiece key={index} piece={piece} />
+      })}
+    </div>
+  )
 }
-
-const mapState = (reduxState) => ({
-  totalCorrect: reduxState.blockPuzzle,
-})
-
-export default connect(mapState)(AllPieces)
--- src/components/BlockPuzzle/index.js
+++ src/components/BlockPuzzle/index.js
@@ -1,41 +1,64 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import Board from './board'
 import AllPieces from './allPieces'
 import Destination from './ImageAssets/pawel-czerwinski-F_dg3zc95Jc-unsplash.jpg'
 import BuboContainer from './bubo-container'
 
-export default function BlockPuzzle() {
-  return (
-    <div>
-      <div style={{display: 'flex', justifyContent: 'center', margin: '1em'}}>
-        The bubos need your help to cross a celestial bridge. Use the pieces to
-        fill in the bridge so they can cross to the next planet!
+export class BlockPuzzle extends React.Component {
+  constructor() {
+    super()
+    this.handleWin = this.handleWin.bind(this)
+  }
+
+  handleWin() {
+    console.log('congratulations, you won!')
+  }
+
+  render() {
+    let puzzleText = 'the bubos need your help to cross a celestial bridge'
+    const winCondition = this.props.totalCorrect === 12
+    if (winCondition) {
+      puzzleText = 'you won! congratulations!'
+    }
+
+    return (
+      <div>
+        <div style={{display: 'flex', justifyContent: 'center', margin: '1em'}}>
+          {puzzleText}
+        </div>
+        <div
+          style={{
+            margin: '1em',
+            width: '95vw',
+            height: '95vh',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+          }}
+        >
+          <div style={{width: '25vw'}}>
+            <BuboContainer />
+          </div>
+          <div>
+            <Board />
+            <AllPieces />
+          </div>
+          <div>
+            <img
+              alt="your destination planet, a dark purple and blue sphere"
+              src={Destination}
+              style={{width: '25vw'}}
+            />
+          </div>
+        </div>
       </div>
-      <div
-        style={{
-          margin: '1em',
-          width: '95vw',
-          height: '95vh',
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-        }}
-      >
-        <div style={{width: '25vw'}}>
-          <BuboContainer />
-        </div>
-        <div>
-          <Board />
-          <AllPieces />
-        </div>
-        <div>
-          <img
-            alt="your destination planet, a dark purple and blue sphere"
-            src={Destination}
-            style={{width: '25vw'}}
-          />
-        </div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
+
+const mapState = (reduxState) => ({
+  totalCorrect: reduxState.blockPuzzle,
+})
+
+export default connect(mapState)(BlockPuzzle)
