0ac256ddaaf4211cf5c4ef7f33e86ef707663ea7
Simone Pincsak <simone.pincsak@gmail.com>
2020-09-03 16:05:53 -0500

    randomizing works, need to troubleshoot hover

---
+++ src/components/create-random-bubo.js
@@ -0,0 +1,46 @@
+export default function createRandomBubo() {
+  const colors = ['purple', 'blue', 'green', 'yellow', 'orange', 'red']
+  const accessories = [
+    'antennae',
+    'hat',
+    'glasses',
+    'monobrow',
+    'bow',
+    'sunglasses',
+    'eyestalk',
+  ]
+  const personalityOne = [
+    'shy',
+    'stubborn',
+    'proud',
+    'assertive',
+    'outspoken',
+    'spontaneous',
+    'soft-spoken',
+  ]
+  const personalityTwo = [
+    'brave',
+    'kind',
+    'confident',
+    'thoughtful',
+    'nurturing',
+    'charaismatic',
+    'patient',
+  ]
+
+  const getRandomTrait = (array) => {
+    const index = Math.floor(Math.random() * Math.floor(array.length))
+    return array[index]
+  }
+
+  const randomBubo = {
+    color: getRandomTrait(colors),
+    accessory: getRandomTrait(accessories),
+    personality: [
+      getRandomTrait(personalityOne),
+      getRandomTrait(personalityTwo),
+    ],
+  }
+
+  return randomBubo
+}
